601a75d021ba6ae5e567e6d1c35c0dfb7b67f975
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 05:27:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,14 @@
 def reverse_each_word(list)
   newList = list.split(' ')
+<<<<<<< HEAD
   newList.collect do |x|
     x.reverse!
   end
   newList.join(' ')
+=======
+  newList.each do |x|
+    x.reverse
+  newList.join(' ')
+  end
+>>>>>>> 1480663a06ccdd5ae9555e54cdb8c74c98d53ff0
 end
